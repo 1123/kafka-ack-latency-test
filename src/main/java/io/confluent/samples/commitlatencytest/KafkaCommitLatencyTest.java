998d477d175aac44be7dce681886140639049581
Benedikt Linse <benedikt@confluent.io>
2022-10-05 09:24:21 +0200

    generating larger random messages; producing with multiple threads

--- src/main/java/io/confluent/samples/commitlatencytest/KafkaCommitLatencyTest.java
+++ src/main/java/io/confluent/samples/commitlatencytest/KafkaCommitLatencyTest.java
@@ -1,6 +1,8 @@
 package io.confluent.samples.commitlatencytest;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.text.RandomStringGenerator;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.CreateTopicsResult;
 import org.apache.kafka.clients.admin.KafkaAdminClient;
@@ -12,7 +14,9 @@ import org.apache.kafka.common.errors.TopicExistsException;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -26,7 +30,9 @@ import java.util.concurrent.Future;
 @Slf4j
 public class KafkaCommitLatencyTest  {
 
-	public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+	public static int numThreads = 16;
+
+	public static void main(String[] args) throws IOException, InterruptedException {
 		Properties properties = new Properties();
 		String e = System.getenv("ENVIRONMENT");
 		String env = (e == null || e.isEmpty()) ? "local" : e;
@@ -39,14 +45,72 @@ public class KafkaCommitLatencyTest  {
 			log.warn(topicExistsException.getMessage());
 		}
 		try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
-			for (int i = 0; i < 100; i++) {
-				var beforeTime = System.currentTimeMillis();
-				Future<RecordMetadata> result = producer.send(new ProducerRecord<>("test-topic", "foo", "bar"));
-				result.get();
-				var timeDiff = System.currentTimeMillis() - beforeTime;
-				log.info("timeDiff: {}", timeDiff);
+			List<ProducerThread> threads = new ArrayList<>();
+			for (int t = 0; t < numThreads; t++) {
+				ProducerThread producerThread = new ProducerThread(producer);
+				threads.add(producerThread);
+				producerThread.start();
 			}
+			threads.forEach(thread -> {
+				try {
+					thread.join();
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+			});
 		}
+	}
+
+}
+
+@Slf4j
+class ProducerThread extends Thread {
+
+	static final int numMessagesPerBatch = 50;
+	static final int numBatches = 500;
+	static final int numKeyChars = 10;
+	static final int numValueChars = 5 * 1024;
+	static final int batchSleepMs = 10;
+
+	private KafkaProducer<String, String> producer;
+	private RandomStringGenerator randomStringGenerator;
+
+	public ProducerThread(KafkaProducer<String, String> producer) {
+		this.producer = producer;
+		randomStringGenerator =
+				new RandomStringGenerator.Builder()
+						.withinRange('0', 'z')
+						.build();
+	}
+
+	@SneakyThrows
+	public void run() {
+		var totalTimeDiff = 0;
+		for (int i = 0; i < numBatches; i++) {
+			List<Future<RecordMetadata>> futures = new ArrayList<>();
+			var key = randomStringGenerator.generate(numKeyChars);
+			var value = randomStringGenerator.generate(numValueChars);
+			var beforeTime = System.currentTimeMillis();
+			for (int j = 0; j < numMessagesPerBatch; j++) {
+				futures.add(producer.send(
+						new ProducerRecord<>("test-topic", key, value)
+				));
+			}
+			futures.forEach(f -> {
+				try {
+					f.get();
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				} catch (ExecutionException ex) {
+					ex.printStackTrace();
+				}
+			});
+			var timeDiff = System.currentTimeMillis() - beforeTime;
+			totalTimeDiff += timeDiff;
+			log.info("timeDiff: {}", timeDiff);
+			Thread.sleep(batchSleepMs);
+		}
+		log.info("average wait: {}", totalTimeDiff / numBatches);
 	}
 
 }
